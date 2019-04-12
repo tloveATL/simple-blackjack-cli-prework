4b073f2a42e9f138960f2809928090b5983859c3
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-12 18:28:15 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,9 +35,16 @@ end
 
 def hit?(card_total)
   prompt_user
-  get_user_input
-  
-
+  user_input = get_user_input
+  while user_input != "h" && user_input != "s"
+    invalid_command
+    prompt_user
+    user_input = get_user_input
+  end
+  if user_input == "h"
+    card_total += deal_card
+  end
+  return card_total
 end
 
 def invalid_command
