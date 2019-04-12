ddec9591e28824f05c32764b574d3920378242c1
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-12 18:55:59 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -59,11 +59,11 @@ def runner
   welcome
   initial_round
   
-  until card_total > 21
-  hit?(card_total)
-  display_card_total(card_total)
-  
-
+    until card_total > 21
+    hit?(card_total)
+    display_card_total(card_total)
+    end
+  end_game(card_total)  
   
 end
     
